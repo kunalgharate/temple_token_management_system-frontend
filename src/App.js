915b18d1f45cde0f360f3db1b9f73c19ff6afa5e
Kunal Gharate <kunalgharate@gmail.com>
2025-10-19 01:03:48 +0530

    Add route debugging to identify issue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import TokenForm from './TokenForm';
 import './App.css';
 
 function App() {
+  const location = useLocation();
+  console.log('Current location:', location.pathname);
+  
   return (
     <Routes>
       <Route path="/fill-details/:tokenNumber" element={<TokenForm />} />
       <Route path="/" element={<div style={{padding: '20px', textAlign: 'center'}}>
         <h1>Temple Token Management</h1>
         <p>Please scan your token QR code</p>
-        <p>Test URL: <a href="/fill-details/T-20251018-601">/fill-details/T-20251018-601</a></p>
+        <p>Current path: {location.pathname}</p>
+        <p>Test URL: <a href="/fill-details/T-20251018-235">/fill-details/T-20251018-235</a></p>
+      </div>} />
+      <Route path="*" element={<div style={{padding: '20px', textAlign: 'center'}}>
+        <h1>Route Debug</h1>
+        <p>Current path: {location.pathname}</p>
+        <p>No route matched</p>
       </div>} />
     </Routes>
   );
