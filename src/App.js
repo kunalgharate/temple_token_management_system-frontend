72202ba9260c7706cd75e53320c42b87a2b7ea28
Kunal Gharate <kunalgharate@gmail.com>
2025-10-19 00:37:31 +0530

    Fix React routing and build process

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
   return (
     <Routes>
       <Route path="/fill-details/:tokenNumber" element={<TokenForm />} />
-      <Route path="/" element={<div>Please scan your token QR code</div>} />
+      <Route path="/" element={<div style={{padding: '20px', textAlign: 'center'}}>
+        <h1>Temple Token Management</h1>
+        <p>Please scan your token QR code</p>
+        <p>Test URL: <a href="/fill-details/T-20251018-601">/fill-details/T-20251018-601</a></p>
+      </div>} />
     </Routes>
   );
 }
